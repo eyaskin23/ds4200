c1c528ceccf05680f1250d5b79ebfbf45b6e7dae
eyaskin <yaskin.e@northeastern.edu>
2024-10-25 14:04:53 -0400

    updating templete

--- templete.js
+++ templete.js
@@ -13,24 +13,20 @@ iris.then(function(data) {
 
     // Define the dimensions and margins for the SVG
     const margin = { top: 20, right: 30, bottom: 40, left: 40},
-        width = 500 - margin.left - margin.right;
-        height = 400 - margin.top - margin.bottom;
+          width = 500 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
 
     // Create the SVG container
-    const svg = d3.select("body").append("Svg")
+    const svg = d3.select("body").append("svg") // Corrected tag name
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-    // Set up scales for x and y axes
-    // d3.min(data, d => d.bill_length_mm)-5
 
+    // Set up scales for x and y axes
     const xScale = d3.scaleLinear() 
         .domain([d3.min(data, d => d.PetalLength) - 1, d3.max(data, d => d.PetalLength) + 1])
         .range([0, width]);
-
-    // Add scales     
 
     const yScale = d3.scaleLinear() 
         .domain([d3.min(data, d => d.PetalWidth) - 1, d3.max(data, d => d.PetalWidth) + 1])
@@ -39,38 +35,35 @@ iris.then(function(data) {
     const colorScale = d3.scaleOrdinal()
         .domain(data.map(d => d.Species))
         .range(d3.schemeCategory10);
-    
 
     // Add circles for each data point
     svg.selectAll("circle")
         .data(data)
         .enter().append("circle")
         .attr("cx", d => xScale(d.PetalLength))
-        .attr("cy", d => yScale(d.PetalLength))
+        .attr("cy", d => yScale(d.PetalWidth)) // Corrected to use PetalWidth for y-axis
         .attr("r", 5)
-        .style("fill", d => colorScale(d.Species))
+        .style("fill", d => colorScale(d.Species));
 
-    
-    // Add x-axis label
+    // Add x-axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(xScale))
         .append("text")
-        .attr("x", width)
-        .attr("y", -10)
-        .style("text-anchor", "end")
+        .attr("x", width / 2)
+        .attr("y", 30) // Adjusted for better visibility
+        .style("text-anchor", "middle")
         .text("Petal Length");
-    
 
-    // Add y-axis label
+    // Add y-axis
     svg.append("g")
         .call(d3.axisLeft(yScale))
         .append("text")
-        .attr("x", 6)
-        .attr("y", 6)
-        .style("text-anchor", "start")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -30) // Adjusted for better visibility
+        .style("text-anchor", "middle")
         .text("Petal Width");
-    
 
     // Add legend
     const legend = svg.selectAll(".legend")
@@ -80,17 +73,15 @@ iris.then(function(data) {
         .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
 
     legend.append("rect")
-        .attr("x", width + 10)
+        .attr("x", width - 80) // Adjusted x position to fit within the SVG
         .attr("width", 18)
         .attr("height", 18)
         .style("fill", colorScale);
     
     legend.append("text")
-        .attr("x", width + 34)
+        .attr("x", width - 54) // Adjusted x position to align with the rectangle
         .attr("y", 9)
         .attr("dy", ".35em")
         .style("text-anchor", "start")
         .text(d => d);
-
 });
-
